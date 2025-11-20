fd3c67bf39780e69c5070925cd51e28923814d87
Kiwada <mamakaiofrep@gmail.com>
2025-11-20 00:51:40 -0300

    feat(frontend): Finalizado

--- luz-e-cena/src/components/Footer/FooterLogo.tsx
+++ luz-e-cena/src/components/Footer/FooterLogo.tsx
@@ -1,13 +1,13 @@
-import Logo from "../Logo/Index"
-import styles from "./Footer.module.css"
+import Logo from "../Logo/Index";
+import styles from "./Footer.module.css";
 
 const FooterLogo = () => {
   return (
     <div className={styles.logo}>
-        <Logo src="logo_branco.png" alt="Logo" />
-        <p className={styles.titulo}>a Realidade Encontra a Fantasia !</p>
+      <Logo src="./logo_branco.png" alt="Logo" />
+      <p>Onde a realidade encontra a fantasia!</p>
     </div>
-  )
-}
+  );
+};
 
-export default FooterLogo
+export default FooterLogo;
--- luz-e-cena/src/components/Footer/index.tsx
+++ luz-e-cena/src/components/Footer/index.tsx
@@ -1,16 +1,24 @@
-import FooterInfo from "./FooterInfo"
-import FooterLogo from "./FooterLogo"   
-import styles from "./Footer.module.css"
+import FooterInfo from "./FooterInfo";
+import FooterLogo from "./FooterLogo";
+import styles from "./Footer.module.css";
+import FooterDevelopedBy from "./FooterDevelop";
+import FooterInstitucional from "./FooterInstitucional";
+import FooterSocial from "./FooterSocial";
 
 const Footer = () => {
   return (
-  <footer className={styles.rodape}>
-    <div className={styles.rodape_informacoes}>             
-    <FooterLogo />
-    <FooterInfo />
-    </div>
-  </footer>
+    <>
+      <footer className={styles.rodape}>
+        <div className={styles.rodape_informacoes}>
+          <FooterLogo />
+          <FooterInfo />
+          <FooterInstitucional />
+          <FooterSocial />
+        </div>
+      </footer>
+      <FooterDevelopedBy />
+    </>
   );
 };
 
-export default Footer
+export default Footer;
